617d56184e4089a2985e0f8a3cb7777cc9ce887a
Jimmy Leung <jimmyjimmy26282@gmail.com>
2024-07-13 13:09:18 +0800

    v1.2

--- app/(root)/discover/details/page.tsx
+++ app/(root)/discover/details/page.tsx
@@ -1,5 +1,5 @@
 'use client'
-import React, { Suspense, useEffect, useState } from 'react'
+import React, { useEffect, useState, Suspense } from 'react'
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 import { useSearchParams } from 'next/navigation'
 import { cn } from '@/lib/utils';
@@ -11,7 +11,7 @@ import { placeDetails } from '@/lib/actions/fourSquareAPI';
 import { placeDetailsFields } from '@/constants';
 import { textSearch } from '@/lib/actions/googleAPI';
 
-export default function page() {
+const DetailsPage = () => {
   const searchParams = useSearchParams();
   const id = searchParams.get('id');
   const [type, setType] = useState<'details' | 'reviews'>('details');
@@ -36,7 +36,6 @@ export default function page() {
     }
   }
 
-  // do fetch reviews from google place
   useEffect(() => {
     const fromDefault = getResultsItemById('defaultItems', id!);
     const fromResults = getResultsItemById('resultsItems', id!);
@@ -52,58 +51,64 @@ export default function page() {
   }, [resultsItem, itemDetails]);
 
   return (
-    <Suspense>
-      <div className='flex 3xl:justify-center gap-8 w-full max-lg:pb-28 max-md:flex-col pt-5'>
-        {
-          resultsItem &&
-          <>
-            <PhotosCard item={resultsItem} photos={itemDetails?.photos || []}/>
+    <div className='flex 3xl:justify-center gap-8 w-full max-lg:pb-28 max-md:flex-col pt-5'>
+      {
+        resultsItem &&
+        <>
+          <PhotosCard item={resultsItem} photos={itemDetails?.photos || []}/>
 
-            <div className='w-full'>
-              <Tabs 
-                defaultValue='details' 
-                className='flex flex-col items-start max-md:items-center w-full relative'
-              >
-                <TabsList className='absolute top-0 left-0 md:justify-start w-full p-0 mb-7 bg-customWhite-100 z-40 rounded-none'>
-                  <TabsTrigger 
-                    value='details'
-                    className={cn('details-tab', {
-                      'details-tab-active': type === 'details'
-                    })}
-                    onClick={() => setType('details')}
-                  >
-                    Details
-                  </TabsTrigger>
+          <div className='w-full'>
+            <Tabs 
+              defaultValue='details' 
+              className='flex flex-col items-start max-md:items-center w-full relative'
+            >
+              <TabsList className='absolute top-0 left-0 md:justify-start w-full p-0 mb-7 bg-customWhite-100 z-40 rounded-none'>
+                <TabsTrigger 
+                  value='details'
+                  className={cn('details-tab', {
+                    'details-tab-active': type === 'details'
+                  })}
+                  onClick={() => setType('details')}
+                >
+                  Details
+                </TabsTrigger>
 
-                  <TabsTrigger 
-                    value='reviews'
-                    className={cn('details-tab', {
-                      'details-tab-active': type === 'reviews'
-                    })}
-                    onClick={() => setType('reviews')}
-                  >
-                    Reviews
-                  </TabsTrigger>
-                </TabsList>
+                <TabsTrigger 
+                  value='reviews'
+                  className={cn('details-tab', {
+                    'details-tab-active': type === 'reviews'
+                  })}
+                  onClick={() => setType('reviews')}
+                >
+                  Reviews
+                </TabsTrigger>
+              </TabsList>
 
-                <TabsContent value="details" className='details-content'>
-                  {
-                    itemDetails &&
-                    <Details itemDetails={itemDetails} extraData={extraData} />
-                  }
-                </TabsContent>
-                <TabsContent value="reviews" className='details-content'>
-                  {
-                    extraData?.reviews ?
-                    <Reviews reviews={extraData?.reviews} /> :
-                    'No Reviews'
-                  }
-                </TabsContent>
-              </Tabs>
-            </div>
-          </>
-        }
-      </div>
-    </Suspense>
+              <TabsContent value="details" className='details-content'>
+                {
+                  itemDetails &&
+                  <Details itemDetails={itemDetails} extraData={extraData} />
+                }
+              </TabsContent>
+              <TabsContent value="reviews" className='details-content'>
+                {
+                  extraData?.reviews ?
+                  <Reviews reviews={extraData?.reviews} /> :
+                  'No Reviews'
+                }
+              </TabsContent>
+            </Tabs>
+          </div>
+        </>
+      }
+    </div>
   )
 }
+
+const WrappedDetailsPage = () => (
+  <Suspense fallback={<div>Loading...</div>}>
+    <DetailsPage />
+  </Suspense>
+);
+
+export default WrappedDetailsPage;
