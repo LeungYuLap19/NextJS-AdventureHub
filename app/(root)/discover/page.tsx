7f7815727473ae2ab1cf71410f83d43c47068599
Jimmy Leung <jimmyjimmy26282@gmail.com>
2024-07-13 13:12:56 +0800

    v1.2

--- app/(root)/discover/page.tsx
+++ app/(root)/discover/page.tsx
@@ -1,15 +1,15 @@
 'use client'
-import Header from '@/components/discover/Header'
-import Results from '@/components/discover/Results'
-import SearchInput from '@/components/discover/SearchInput'
-import SearchTab from '@/components/discover/SearchTab'
-import Subtitle from '@/components/discover/Subtitle'
-import { searchTabs } from '@/constants'
-import { getFromLocalstorage } from '@/lib/actions/localStorage.actions'
-import { useSearchParams } from 'next/navigation'
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, Suspense } from 'react';
+import Header from '@/components/discover/Header';
+import Results from '@/components/discover/Results';
+import SearchInput from '@/components/discover/SearchInput';
+import SearchTab from '@/components/discover/SearchTab';
+import Subtitle from '@/components/discover/Subtitle';
+import { searchTabs } from '@/constants';
+import { getFromLocalstorage } from '@/lib/actions/localStorage.actions';
+import { useSearchParams } from 'next/navigation';
 
-export default function page() {
+const DiscoverPage = () => {
   const [activeTab, setActiveTab] = useState<SearchTabParams>(searchTabs[0]);
   const [subtitle, setSubtitle] = useState<string>('Popular places around you');
   const searchParams = useSearchParams();
@@ -19,42 +19,41 @@ export default function page() {
     if (type === 'results') {
       const subtitle = getFromLocalstorage<string>('resultsSubtitle');
       setSubtitle(subtitle[0]);
-    }
-    else {
+    } else {
       setSubtitle('Popular places around you');
     }
   }, [type]);
 
   return (
     <div className='flex flex-col gap-7 pt-[60px] max-sm:pt-[30px] max-lg:pb-28'>
-      <Header 
-        title={<>Where do<br />you want to go?</>}
-      />
+      <Header title={<>Where do<br />you want to go?</>} />
 
       <div className='flex gap-[20px] max-md:flex-col md:mb-12'>
         <SearchInput activeTab={activeTab} setSubtitle={setSubtitle} />
-        <Subtitle
-          title={<>Categories</>}
-          style={'md:hidden'}
-        />
+        <Subtitle title={<>Categories</>} style={'md:hidden'} />
         <div className='flex items-center h-[50px] max-md:h-10 overflow-auto'>
-          {
-            searchTabs.map((tab: SearchTabParams) => (
-                <SearchTab 
-                  key={tab.label}
-                  tab={tab}
-                  activeTab={activeTab}
-                  setActiveTab={setActiveTab}
-                />
-            ))
-          }
+          {searchTabs.map((tab: SearchTabParams) => (
+            <SearchTab 
+              key={tab.label}
+              tab={tab}
+              activeTab={activeTab}
+              setActiveTab={setActiveTab}
+            />
+          ))}
         </div>
       </div>
 
       <div className='flex flex-col w-full gap-12'>
         <Results subtitle={subtitle} />
       </div>
-      
     </div>
-  )
-}
+  );
+};
+
+const WrappedDiscoverPage = () => (
+  <Suspense fallback={<div>Loading...</div>}>
+    <DiscoverPage />
+  </Suspense>
+);
+
+export default WrappedDiscoverPage;
